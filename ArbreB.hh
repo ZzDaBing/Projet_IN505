e4580dae0769aae9a42df79d01c89303b7ea0725
Mélanie Bonnet <melanie.bonnet@ens.uvsq.fr>
2020-11-20 12:37:52 +0100

    ajoue de test et une petite partie de constructeur

--- ArbreB.hh
+++ ArbreB.hh
@@ -16,9 +16,31 @@ public:
 		this->fd=NULL;
 	}
 	
-	ArbreB(char l, int o, ArbreB f) : Sommet(l,o){
-		this->fg=&f;
-		this->fd=NULL;
+	// ArbreB(char l, int o, ArbreB f) : Sommet(l,o){
+	// 	this->fg=&f;
+	// 	this->fd=NULL;
+	// }
+
+	ArbreB(int a ,  ArbreB f) : Sommet(){
+		if (a){
+			this->fd=&f;
+			this->fg=NULL;
+		}
+		else {
+			this->fg=&f;
+			this->fd=NULL;
+		}
+	}
+
+	ArbreB(char l, int o, int a ,  ArbreB f) : Sommet(l,o){
+		if (a){
+			this->fd=&f;
+			this->fg=NULL;
+		}
+		else {
+			this->fg=&f;
+			this->fd=NULL;
+		}
 	}
 	
 	ArbreB(ArbreB fg, ArbreB fd) : Sommet(){
@@ -35,6 +57,33 @@ public:
 	
 	ArbreB* getfg(){return this->fg;}
 	ArbreB* getfd(){return this->fd;}
+
+	//fonction qui renvoie toute les lettres et occurences par profondeur
+
+	int rechAllElem(int prof, char const * nomArbre, int gORd){
+
+		if (gORd == -1)
+			std::cout << "Sommet de l'arbre "<<  nomArbre <<  " egale a : '" << this->getLettre() << "' et son occurence a : " << this->getOccurence() << std::endl;
+
+		else if (gORd)
+			std::cout << "Element trouvé a la profondeur "<< prof << " : '" << this->getLettre() << "' et son occurence a : " << this->getOccurence() << " dans l'abre " << nomArbre << " a droite" << std::endl;
+
+		else 
+			std::cout << "Element trouvé a la profondeur "<< prof << " : '" << this->getLettre() << "' et son occurence a : " << this->getOccurence() << " dans l'abre " << nomArbre << " a gauche" << std::endl;
+
+
+		if(fg and fd)
+			return fg->rechAllElem(prof+1,nomArbre,0) + fd->rechAllElem(prof+1,nomArbre,1);
+		else if(fg){
+			return fg->rechAllElem(prof+1,nomArbre,0);
+		}
+		else if(fd){
+			return fd->rechAllElem(prof+1,nomArbre,1);
+		}
+
+		else return 0;
+	}
+
 	
 	//fonction de recherche renvoyant un boolean
 	bool rechElem(char l){
--- Main.cc
+++ Main.cc
@@ -6,29 +6,73 @@ int main(){
 	
 	ArbreB A('a', 3);
 	ArbreB b('b', 2);
-	ArbreB c('c', 5);
+	ArbreB c('c', 5,A,b);
 	ArbreB Ab(A, b);
+	ArbreB Ac(1, b);
 	ArbreB ABC(Ab,c);
-	std::cout << A.getLettre() << " " << A.getOccurence() << " " << std::endl;
-	Ab.getfg()->setLettre('H');
-	Ab.getfg()->setOccurence(7);
-	std::cout << A.getLettre() << " " << A.getOccurence() << " " << std::endl;
+	ArbreB ABAB(Ab,ABC);
+
+	c.getfg()->setLettre('H');
+	c.getfg()->setOccurence(7);
+	c.getfd()->setLettre('B');
+	c.getfd()->setOccurence(9);
+
+	// c.rechAllElem(0,"c",-1);
+	// b.rechAllElem(0,"b",-1);
+	// A.rechAllElem(0,"A",-1);
+	Ac.rechAllElem(0,"Ac",-1);
+	
+	// std::cout << A.getLettre() << " " << A.getOccurence() << " " << std::endl;
+	// Ab.rechAllElem(0,"Ab",-1);
+	// std::cout << std::endl;
+
+	// Ab.rechAllElem(0,"Ab",-1);
+	// std::cout << std::endl;
+
+	// Ab.getfg()->setLettre('H');
+	// Ab.getfg()->setOccurence(7);
+
+
+	// Ab.rechAllElem(0,"Ab",-1);
+	// std::cout << std::endl;
+	// std::cout << Ab.getLettre() << " " << Ab.getOccurence() << " " << std::endl;
 	//ArbreB Ab(A, b);
 		
 	//en void
-	Ab.rechercheElem('H');
-	Ab.rechercheElem('b');
-	Ab.rechercheElem('a');
+	// Ab.rechercheElem('H');
+	// Ab.rechercheElem('b');
+	// Ab.rechercheElem('a');
+
+	//rechercher tout les elements dans l'arbre 
+
+	// A.rechAllElem(0,"A",-1);
+	// std::cout << std::endl;
+	// b.rechAllElem(0,"B",-1);
+	// std::cout << std::endl;
+	// c.rechAllElem(0,"C",-1);
+	// std::cout << std::endl;
+	// Ab.rechAllElem(0,"Ab",-1);
+	// std::cout << std::endl;
+	// ABC.rechAllElem(0,"ABC",-1);
+	// std::cout << std::endl;
+	// ABAB.rechAllElem(0,"ABAB",-1);
+	// std::cout << std::endl;
+
+
 	
 	//en booleen
-	if(ABC.rechElem('H'))
-		std::cout << "Element H trouvé " << std::endl;
-	if(ABC.rechElem('b'))
-		std::cout << "Element b trouvé " << std::endl;
-	if(ABC.rechElem('a'))
-		std::cout << "Element a trouvé " << std::endl;
-	if(ABC.rechElem('c'))
-		std::cout << "Element c trouvé " << std::endl;
+	// if(ABC.rechElem('H'))
+	// 	std::cout << "Element H trouvé " << std::endl;
+	// if(ABC.rechElem('b'))
+	// 	std::cout << "Element b trouvé " << std::endl;
+	// if(ABC.rechElem('a'))
+	// 	std::cout << "Element a trouvé " << std::endl;
+	// if(ABC.rechElem('c'))
+	// 	std::cout << "Element c trouvé " << std::endl;
+
+	// std::cout << ABC.getLettre() <<std::endl;
+
+
 	
 	return 0;
 }
